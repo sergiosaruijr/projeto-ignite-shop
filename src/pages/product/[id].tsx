390b88bafbe7bea7c9fed8b5940a18906e6e8291
sergiosaruijr <sergiosaruijr@gmail.com>
2023-06-22 23:05:41 -0300

    Inicio

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+
+export default function Product() {
+  // const { query } = useRouter
+ 
+  return(
+    <h1>Product</h1>
+  )
+}
