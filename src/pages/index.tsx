e341f3abba68e0dc9fd4c0fbf7e92f53958414ca
sergiosaruijr <sergiosaruijr@gmail.com>
2023-06-27 22:31:54 -0300

    Estrutura - Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,27 @@ import { HomeContainer, Product } from '@/styles/pages/home';
 import Image from 'next/image';
 import React from 'react';
 
+import { useKeenSlider } from 'keen-slider/react'
+
 import camiseta1 from '../assests/1.png';
 import camiseta2 from '../assests/2.png';
-// import camiseta3 from '../assests/3.png';
+import camiseta3 from '../assests/3.png';
+
+import 'keen-slider/keen-slider.min.css';
+import { stripe } from '@/lib/stripe';
+import { GetServerSideProps } from 'next';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3, 
+      spacing: 48,
+    }
+  })
+
   return (
-   <HomeContainer>
-    <Product>
+   <HomeContainer ref={sliderRef} className='keen-slider'>
+    <Product className='keen-slider__slide'>
       <Image src={camiseta1} width={520} height={480} alt=""/>
 
       <footer>
@@ -18,7 +31,25 @@ export default function Home() {
       </footer>
     </Product>
 
-    <Product>
+    <Product className='keen-slider__slide'>
+      <Image src={camiseta2} width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>Camiseta X </strong>
+        <span>R$ 79,00</span>
+      </footer>
+    </Product>
+
+    <Product className='keen-slider__slide'>
+      <Image src={camiseta3} width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>Camiseta X </strong>
+        <span>R$ 79,00</span>
+      </footer>
+    </Product>
+
+    <Product className='keen-slider__slide'>
       <Image src={camiseta2} width={520} height={480} alt=""/>
 
       <footer>
@@ -28,4 +59,16 @@ export default function Home() {
     </Product>
    </HomeContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // await new Promise(resolve => setTimeout(resolve, 2000))
+  const response = await stripe.products.list()
+
+  console.log(response.data)
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,7 +3,7 @@ import { styled } from '..';
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
+  // gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px)/2))',
   marginLeft: 'auto',
@@ -17,7 +17,7 @@ export const Product = styled('a', {
 
   background: 'linear-gradient(180deg, #1ea438 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
+  // padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
