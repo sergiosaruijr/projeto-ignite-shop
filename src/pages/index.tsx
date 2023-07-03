a5a041334673ef81bbbd46cbb0aa64e97d46a71a
sergiosaruijr <sergiosaruijr@gmail.com>
2023-07-03 20:22:10 -0300

    Primeira finalização/ adicionando HEAD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { HomeContainer, Product } from '@/styles/pages/home';
 import Image from 'next/image';
 import React from 'react';
-import Link from 'next/link'
+import Head from 'next/head'
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -29,26 +29,31 @@ export default function Home({products}: HomeProps) {
   })
 
   return (
-   <HomeContainer ref={sliderRef} className='keen-slider'>
-    {products.map(product => {
-      return(
-        <Product 
-          className='keen-slider__slide' 
-          href={`/product/${product.id}`} 
-          key={product.id} 
-          // prefetch={false}
-        >
-          <Image src={product.imageUrl} width={520} height={480} alt=""/>
-          
-          <footer>
-            <strong>{product.name} </strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
-      )
-    })}
+    <>
+      <Head>
+        <title>Ignite Shop</title>
+      </Head>
 
-   </HomeContainer>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map(product => {
+          return(
+            <Product 
+              className='keen-slider__slide' 
+              href={`/product/${product.id}`} 
+              key={product.id} 
+              // prefetch={false}
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt=""/>
+              
+              <footer>
+                <strong>{product.name} </strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
